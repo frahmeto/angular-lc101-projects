bee06f2b37093136264ce04f048c47c9494c14fa
Fidel Rahmeto <fidyod@gmail.com>
2021-12-13 21:35:27 -0600

    finished lesson3 ex

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -22,8 +22,14 @@ export class CandidatesComponent implements OnInit {
 
   ngOnInit() {
   }
-
   // Code the addToCrew function here:
+   //TODO: complete the function!
+  addToCrew(person: object) {
+    if(!this.crew.includes(person)){
+      this.crew.push(person);
+    }
+   
+ }
 
 
   // BONUS: Code the changeMissionName function here:
